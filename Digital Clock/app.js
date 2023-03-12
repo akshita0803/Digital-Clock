8e43f5fb79838e2d8751fa35b080cde52b94412d
akshita0803 <127326555+akshita0803@users.noreply.github.com>
2023-03-12 20:45:50 +0530

    Add files via upload

---
+++ Digital Clock/app.js
@@ -0,0 +1,37 @@
+// console.log("fgejhca");
+const hourEl=document.getElementById("hour");
+const minuteEl=document.getElementById("minutes");
+const secondsEl=document.getElementById("seconds");
+const ampmEl=document.getElementById("ampm");
+
+function updateClock(){
+
+let h= new Date().getHours();
+let m= new Date().getMinutes();
+let s= new Date().getSeconds();
+let ampm="AM";
+
+
+if(h>12){
+    h=h-12;
+    ampm="PM";
+}
+h = h < 10 ? "0"+h : h;
+m = m < 10 ? "0"+m : m;
+s = s < 10 ? "0"+s : s;
+
+
+hourEl.innerHTML=h;
+minuteEl.innerHTML=m;
+secondsEl.innerHTML=s;
+
+
+
+setTimeout(()=>{
+    updateClock();
+},1000);
+}
+
+
+
+updateClock();
